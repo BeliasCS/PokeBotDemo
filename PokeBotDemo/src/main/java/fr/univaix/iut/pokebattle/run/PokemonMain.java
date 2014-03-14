6772324cf43e827d5e69a898a7a3cf9bf75d23bc
arthur <arthur.cavaille@gmail.com>
2014-03-14 16:10:14 +0100

    correction twitter + A1,A2

---
+++ PokeBotDemo/src/main/java/fr/univaix/iut/pokebattle/run/PokemonMain.java
@@ -0,0 +1,23 @@
+package fr.univaix.iut.pokebattle.run;
+
+import fr.univaix.iut.pokebattle.bot.JudgeBot;
+import fr.univaix.iut.pokebattle.bot.PokeBot;
+import fr.univaix.iut.pokebattle.tuse.TUSEException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PokemonMain {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PokemonMain.class);
+
+    private PokemonMain() {
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            BotRunner.runBot(new JudgeBot(), "twitter.properties");
+        } catch (TUSEException e) {
+            LOGGER.error("Erreur sérieuse dans le BotRunner", e);
+        }
+    }
+}
